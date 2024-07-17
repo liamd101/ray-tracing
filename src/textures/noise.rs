56e1dc2ac38704b22e4239225d27123ea64744ef
liamd101 <liamdodds101@gmail.com>
2024-07-17 12:56:43 -0400

    Random vectors instead of floats

--- src/textures/noise.rs
+++ src/textures/noise.rs
@@ -1,7 +1,7 @@
 use crate::Texture;
 
 pub struct Perlin {
-    point_count: usize,
+    rand_vec: Vec<crate::Vec3>,
     rand_float: Vec<f32>,
     perm_x: Vec<i32>,
     perm_y: Vec<i32>,
@@ -11,12 +11,15 @@ pub struct Perlin {
 impl Perlin {
     pub fn new() -> Self {
         let point_count = 256;
+        let rand_vec = (0..point_count)
+            .map(|_| crate::vec3::unit_vector(crate::Vec3::random_range(-1.0, 1.0)))
+            .collect();
         let rand_float = (0..point_count).map(|_| rand::random::<f32>()).collect();
         let perm_x = Self::perlin_generate_perm();
         let perm_y = Self::perlin_generate_perm();
         let perm_z = Self::perlin_generate_perm();
         Self {
-            point_count,
+            rand_vec,
             rand_float,
             perm_x,
             perm_y,
@@ -25,21 +28,19 @@ impl Perlin {
     }
 
     pub fn noise(&self, p: &crate::Point3) -> f32 {
-        let mut u = p.x() - p.x().floor();
-        let mut v = p.y() - p.y().floor();
-        let mut w = p.z() - p.z().floor();
-        u = u * u * (3.0 - 2.0 * u);
-        v = v * v * (3.0 - 2.0 * v);
-        w = w * w * (3.0 - 2.0 * w);
+        let u = p.x() - p.x().floor();
+        let v = p.y() - p.y().floor();
+        let w = p.z() - p.z().floor();
 
         let i = p.x().floor() as i32;
         let j = p.y().floor() as i32;
         let k = p.z().floor() as i32;
-        let mut c: [[[f32; 2]; 2]; 2] = [[[0.0; 2]; 2]; 2];
+        let mut c: [[[crate::Vec3; 2]; 2]; 2] = [[[crate::Vec3::default(); 2]; 2]; 2];
+
         for di in 0..2 {
             for dj in 0..2 {
                 for dk in 0..2 {
-                    c[di as usize][dj as usize][dk as usize] = self.rand_float[(self.perm_x
+                    c[di as usize][dj as usize][dk as usize] = self.rand_vec[(self.perm_x
                         [((i + di) & 255) as usize]
                         ^ self.perm_y[((j + dj) & 255) as usize]
                         ^ self.perm_z[((k + dk) & 255) as usize])
@@ -51,15 +52,20 @@ impl Perlin {
         Self::trilinear_interp(c, u, v, w)
     }
 
-    fn trilinear_interp(c: [[[f32; 2]; 2]; 2], u: f32, v: f32, w: f32) -> f32 {
+    fn trilinear_interp(c: [[[crate::Vec3; 2]; 2]; 2], u: f32, v: f32, w: f32) -> f32 {
+        let uu = u * u * (3.0 - 2.0 * u);
+        let vv = v * v * (3.0 - 2.0 * v);
+        let ww = w * w * (3.0 - 2.0 * w);
+
         let mut accum = 0.0;
         for i in 0..2 {
             for j in 0..2 {
                 for k in 0..2 {
-                    accum += (i as f32 * u + (1.0 - i as f32) * (1.0 - u))
-                        * (j as f32 * v + (1.0 - j as f32) * (1.0 - v))
-                        * (k as f32 * w + (1.0 - k as f32) * (1.0 - w))
-                        * c[i][j][k];
+                    let weight_v = crate::Vec3::new(u - i as f32, v - j as f32, w - k as f32);
+                    accum += (i as f32 * uu + (1.0 - i as f32) * (1.0 - uu))
+                        * (j as f32 * vv + (1.0 - j as f32) * (1.0 - vv))
+                        * (k as f32 * ww + (1.0 - k as f32) * (1.0 - ww))
+                        * crate::vec3::dot(c[i][j][k], weight_v);
                 }
             }
         }
@@ -94,6 +100,6 @@ impl PerlinNoise {
 }
 impl Texture for PerlinNoise {
     fn value(&self, u: f32, v: f32, p: &crate::Point3) -> crate::Color {
-        crate::Color::new(1.0, 1.0, 1.0) * self.noise.noise(&(*p * self.scale))
+        crate::Color::new(1.0, 1.0, 1.0) * 0.5 * (1.0 + self.noise.noise(&(*p * self.scale)))
     }
 }
