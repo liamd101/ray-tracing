c931346be809dd8819056736f0bf2a976787ae7a
Liam Dodds <liamdodds101@gmail.com>
2025-06-02 18:57:14 -0400

    add final scene for second book

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use ray_tracing::{
-    new_box, utils, BvhNode, Camera, Checkerboard, Color, Dielectric, DiffuseLight, HittableList,
-    Lambertian, Material, Metal, Point3, Quadrilateral as Quad, RotateY, SolidColor, Sphere,
-    Translate, Vec3, PerlinNoise
+    new_box,
+    utils::{self, random_double_range},
+    BvhNode, Camera, Checkerboard, Color, Dielectric, DiffuseLight, HittableList, Lambertian,
+    Material, Metal, PerlinNoise, Point3, Quadrilateral as Quad, RotateY, SolidColor, Sphere,
+    Translate, Vec3,
 };
 
 use clap::{Parser, Subcommand};
@@ -361,6 +363,106 @@ fn perlin_spheres(image_width: usize) {
     cam.render(&world);
 }
 
+fn final_scene() {
+    let mut boxes1 = HittableList::new();
+    let ground = Lambertian::new(Color::new(0.48, 0.83, 0.53));
+
+    let boxes_per_side = 20;
+    for i in 0..boxes_per_side {
+        for j in 0..boxes_per_side {
+            let w = 100.0;
+            let x0 = -1000.0 + (i as f32) * w;
+            let z0 = -1000.0 + (j as f32) * w;
+            let y0 = 0.0;
+            let x1 = x0 + w;
+            let y1 = random_double_range(1.0, 101.0);
+            let z1 = z0 + w;
+
+            boxes1.add(Rc::new(new_box(
+                Point3::new(x0, y0, z0),
+                Point3::new(x1, y1, z1),
+                Box::new(ground.clone()),
+            )));
+        }
+    }
+    let mut world = HittableList::new();
+    world.add(Rc::new(BvhNode::from_list(boxes1)));
+
+    let light = DiffuseLight::from_color(Color::new(7.0, 7.0, 7.0));
+    world.add(Rc::new(Quad::new(
+        Point3::new(123.0, 554.0, 147.0),
+        Vec3::new(300.0, 0.0, 0.0),
+        Vec3::new(0.0, 0.0, 265.0),
+        Box::new(light),
+    )));
+
+    let center1 = Point3::new(400.0, 400.0, 200.0);
+    let center2 = center1 + Vec3::new(30.0, 0.0, 0.0);
+    let sphere_material = Box::new(Lambertian::new(Color::new(0.7, 0.3, 0.1)));
+    world.add(Rc::new(Sphere::moving(
+        center1,
+        center2,
+        50.0,
+        sphere_material,
+    )));
+    world.add(Rc::new(Sphere::stationary(
+        Point3::new(260.0, 150.0, 45.0),
+        50.0,
+        Box::new(Dielectric::new(1.5)),
+    )));
+    world.add(Rc::new(Sphere::stationary(
+        Point3::new(0.0, 150.0, 145.0),
+        50.0,
+        Box::new(Metal::new(Color::new(0.8, 0.8, 0.9), 1.0)),
+    )));
+    /* TODO
+    let boundary = Sphere::stationary(Point3::new(360.0,150.0,145.0), 70.0, Box::new(Dielectric::new(1.5)));
+    world.add(Rc::new(boundary));
+    world.add(Rc::new(constant_medium(boundary, 0.2, color(0.2, 0.4, 0.9))));
+    boundary = make_shared<sphere>(point3(0,0,0), 5000, make_shared<dielectric>(1.5));
+    world.add(make_shared<constant_medium>(boundary, .0001, color(1,1,1)));
+
+    auto emat = make_shared<lambertian>(make_shared<image_texture>("earthmap.jpg"));
+    world.add(make_shared<sphere>(point3(400,200,400), 100, emat));
+    auto pertext = make_shared<noise_texture>(0.2);
+    world.add(make_shared<sphere>(point3(220,280,300), 80, make_shared<lambertian>(pertext)));
+    */
+
+    let mut boxes2 = HittableList::new();
+    let white = Box::new(Lambertian::new(Color::new(0.73, 0.73, 0.73)));
+    let ns = 1000;
+    for _ in 0..ns {
+        boxes2.add(Rc::new(Sphere::stationary(
+            Point3::random_range(0.0, 165.0),
+            10.0,
+            white.clone(),
+        )));
+    }
+    world.add(Rc::new(Translate::new(
+        Rc::new(RotateY::new(Rc::new(BvhNode::from_list(boxes2)), 15.0)),
+        Vec3::new(-100.0, 270.0, 395.0),
+    )));
+
+    let world: BvhNode = BvhNode::from_list(world);
+
+    let mut cam = Camera::new();
+
+    cam.aspect_ratio = 1.0;
+    cam.image_width = 400;
+    cam.samples_per_pixel = 250;
+    cam.max_depth = 4;
+    cam.background = Color::new(0.0, 0.0, 0.0);
+
+    cam.vfov = 40.0;
+    cam.look_from = Point3::new(478.0, 278.0, -600.0);
+    cam.look_at = Point3::new(278.0, 278.0, 0.0);
+    cam.vup = Vec3::new(0.0, 1.0, 0.0);
+
+    cam.defocus_angle = 0.0;
+
+    cam.render(&world);
+}
+
 #[derive(Parser)]
 #[command(author, version, about, long_about=None)]
 struct Cli {
@@ -379,6 +481,7 @@ enum Command {
     Quads,
     CornellBox,
     PerlinSpheres,
+    FinalTest,
 }
 
 fn main() {
@@ -390,5 +493,6 @@ fn main() {
         Command::Quads => quads(args.image_width),
         Command::CornellBox => cornell_box(args.image_width),
         Command::PerlinSpheres => perlin_spheres(args.image_width),
+        Command::FinalTest => final_scene(),
     }
 }
